c34774b352c39292c8dc2113268b59b7b26edddc
lIlSkaSkaSkalIl <168249736+lIlSkaSkaSkalIl@users.noreply.github.com>
2025-07-23 12:55:27 +0700

    Update logger.py

--- colab_fetcher/bot/logger.py
+++ colab_fetcher/bot/logger.py
@@ -3,7 +3,7 @@ import logging
 def setup_logger():
     logging.basicConfig(
         level=logging.INFO,
-        format="%(levelname)s | %(name)s | %(message)s",
+        format="%(levelname)s %(name)s %(message)s",
         handlers=[logging.StreamHandler()]
     )
     return logging.getLogger("colab_fetcher")
