0dba84b62e4ab9013b6c234ffcf59ada4af06992
lIlSkaSkaSkalIl <168249736+lIlSkaSkaSkalIl@users.noreply.github.com>
2025-07-23 14:25:31 +0700

    Update __main__.py

--- colab_fetcher/__main__.py
+++ colab_fetcher/__main__.py
@@ -1,8 +1,6 @@
 from colab_fetcher.bot.logger import logger
 from colab_fetcher.bot.client import app
-from colab_fetcher.handlers.start_handler import start_handler
-from colab_fetcher.handlers.download_handler import download_command
-from colab_fetcher.handlers.file_handler import handle_file_upload
+from colab_fetcher.handlers.handler import start_handler, download_command, handle_file_upload
 
 if __name__ == "__main__":
     logger.info("Starting the bot...")
