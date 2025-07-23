222d36a00211548fd95c12ab009541bd26b036fa
lIlSkaSkaSkalIl <168249736+lIlSkaSkaSkalIl@users.noreply.github.com>
2025-07-23 14:48:43 +0700

    Update __main__.py

--- colab_fetcher/__main__.py
+++ colab_fetcher/__main__.py
@@ -1,5 +1,5 @@
-from colab_fetcher.bot.logger import logger
-from colab_fetcher.bot.client import app
+from colab_fetcher.utils.logger import logger
+from colab_fetcher.utils.client import app
 from colab_fetcher.handlers.handler import start_handler, download_command, handle_file_upload
 
 if __name__ == "__main__":
