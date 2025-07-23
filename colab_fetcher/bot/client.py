214ed0fcf80dc10b2abde4896acff07b58264fcb
lIlSkaSkaSkalIl <168249736+lIlSkaSkaSkalIl@users.noreply.github.com>
2025-07-23 12:53:05 +0700

    Create client.py

---
+++ colab_fetcher/bot/client.py
@@ -0,0 +1,16 @@
+from pyrogram import Client
+from colab_fetcher import load_credentials
+from colab_fetcher.bot.logger import logger
+
+logger.info("Loading credentials...")
+creds = load_credentials()
+logger.info("Credentials loaded successfully.")
+
+app = Client(
+    "my_bot",
+    api_id=creds["api_id"],
+    api_hash=creds["api_hash"],
+    bot_token=creds["bot_token"]
+)
+
+logger.info("Pyrogram client initialized.")
