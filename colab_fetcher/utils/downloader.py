45b73321403024338e9f04618daa960f2f061c4a
lIlSkaSkaSkalIl <168249736+lIlSkaSkaSkalIl@users.noreply.github.com>
2025-07-25 09:46:50 +0700

    Update downloader.py

--- colab_fetcher/utils/downloader.py
+++ colab_fetcher/utils/downloader.py
@@ -62,7 +62,7 @@ async def download_with_progress(client, message: Message, file_path: str):
             filled = int(14 * percent / 100)
             progress_text = (
                 f"<b>Downloading...</b>\n\n"
-                f"<b>{filename} »\n</b>"
+                f"<b>{filename} »</b>\n\n"
                 f"╭「{'█' * filled}{'░' * (14 - filled)}」 {percent:.1f}%\n"
                 f"├📥 <b>Downloaded »</b> {naturalsize(current)}\n"
                 f"├📁 <b>Total Size »</b> {naturalsize(total)}\n"
