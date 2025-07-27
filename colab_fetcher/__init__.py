7c5d6fa233f7356327f0fdf419a8eb827aefb181
lIlSkaSkaSkalIl <168249736+lIlSkaSkaSkalIl@users.noreply.github.com>
2025-07-27 12:04:04 +0700

    Update __init__.py

--- colab_fetcher/__init__.py
+++ colab_fetcher/__init__.py
@@ -1,11 +1,22 @@
 import os
 import json
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 CONFIG_PATH = "/content/Telegram_Fetcher/colab_fetcher/config/credentials.json"
 
+ERROR_CREDENTIAL_NOT_FOUND = "Credential file not found at {}"
+ERROR_INVALID_JSON = "Invalid JSON format in {}"
+
 def load_credentials():
     if not os.path.exists(CONFIG_PATH):
-        raise FileNotFoundError(f"Credential file not found at {CONFIG_PATH}")
-
-    with open(CONFIG_PATH, "r") as file:
-        return json.load(file)
+        logging.error(ERROR_CREDENTIAL_NOT_FOUND.format(CONFIG_PATH))
+        raise FileNotFoundError(ERROR_CREDENTIAL_NOT_FOUND.format(CONFIG_PATH))
+    try:
+        with open(CONFIG_PATH, "r") as file:
+            logging.info(f"Loaded credentials from {CONFIG_PATH}")
+            return json.load(file)
+    except json.JSONDecodeError as e:
+        logging.error(ERROR_INVALID_JSON.format(CONFIG_PATH) + f": {e}")
+        raise ValueError(ERROR_INVALID_JSON.format(CONFIG_PATH))
